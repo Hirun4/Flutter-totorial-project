3717a575e068c86377fcbf14fd63d27330abf148
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-27 10:46:40 +0530

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
